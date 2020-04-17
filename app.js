94e16c1aeb3c2db571562e3413ab92d7ba79d277
Thomas Lagrange <tplagrange@gmail.com>
2020-04-17 16:29:21 -0400

    Cluster task cycles through list of fonts for screenshot

--- app.js
+++ app.js
@@ -9,59 +9,65 @@ const url    = require('./url.js')
 const { Cluster } = require('puppeteer-cluster')
 
 const seed = "https://github.com/avelino/awesome-go/blob/master/README.md";
-const fonts = [ 'Arial', 'Roboto' ];
+const fonts = [ 'monospace', 'initial', 'sans-serif', 'menlo' ];
 
 run(seed)
 
 async function run(seed) {
-    const cluster = await Cluster.launch({
-      concurrency: Cluster.CONCURRENCY_CONTEXT,
-      maxConcurrency: 20,
-      monitor: true,
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 30,
+    monitor: true,
+    // puppeteerOptions: {
+    //   dumpio: true,
+    // },
+  });
+
+  // TODO: Abstract this to one task per font also
+  await cluster.task(async ({ page, data: url }) => {
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    const text = await page.evaluate('document.querySelector("pre").innerText');
+    fs.writeFile(`data/raw/${hash(url)}.txt`, text, (err) => {
+      if (err) throw err;
     });
 
-    await cluster.task(async ({ page, data: url }) => {
-      await page.goto(url);
+    for (var i = 0; i < fonts.length; i++) {
+      const font = fonts[i];
+      await page.evaluate( (font) => {
+        document.querySelector('pre').style = `font-family: ${font}`
+      }, font);
+      await page.screenshot({path: `data/raw/${hash(url)}-${font}.png`, fullPage: true});
+    }
+  });
 
-      const text = await page.evaluate('document.querySelector("pre").innerText')
-      fs.writeFile(`data/raw/${hash(url)}.txt`, text, (err) => {
-        if (err) throw err;
-      });
-      await page.screenshot({path: `data/raw/${hash(url)}.png`, fullPage: true})
-      await processArray(fonts, async (font) => {
-        await page.addStyleTag({content: `pre {font-family: ${font} !important;}`});
-        await page.screenshot({path: `data/raw/${hash(url)}-${font}.png`, fullPage: true})
-      });
+  // Measure
+  const start = new Date()
+  const hrstart = process.hrtime()
 
-      console.log('done')
-    });
+  let hrefs = new Array()
+  hrefs.push("https://github.com/tplagrange/lf")
+  hrefs.push("https://github.com/tplagrange/fireteam-bot")
+  // const hrefs = await url.getGitHubRepos(seed)
+  // if (hrefs.length == 0) {
+  //   console.log("[ERROR] No GitHub Repos!")
+  //   await cluster.close()
+  //   return process.exit(1)
+  // }
 
-    // Measure
-    const start = new Date()
-    const hrstart = process.hrtime()
+  // const links = await processArray(hrefs, url.getFileLinks)
+  // const files = await processArray(links, prepareFileLink)
+  // processArray(files, (e) => cluster.queue(e))
+  cluster.queue("https://raw.githubusercontent.com/tplagrange/code-scraper/master/app.js")
 
-    let hrefs = new Array()
-    hrefs.push("https://github.com/avelino/awesome-go")
-    // hrefs.push("https://github.com/tplagrange/fireteam-bot")
-    // const hrefs = await url.getGitHubRepos(seed)
-    // if (hrefs.length == 0) {
-    //   console.log("[ERROR] No GitHub Repos!")
-    //   await cluster.close()
-    //   return process.exit(1)
-    // }
-
-    const links = await processArray(hrefs, url.getFileLinks)
-    const files = await processArray(links, prepareFileLink)
-    processArray(files, (e) => cluster.queue(e))
-
-    await cluster.idle()
-    const end = new Date() - start
-    const hrend = process.hrtime(hrstart)
-    console.info(`Processed ${links.length} file URLs`)
-    console.info('Execution time: %dms', end)
-    console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
-    await cluster.close()
-    return process.exit(0)
+  await cluster.idle()
+  const end = new Date() - start
+  const hrend = process.hrtime(hrstart)
+  // console.info(`Processed ${links.length} file URLs`)
+  console.info('Execution time: %dms', end)
+  console.info('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
+  await cluster.close()
+  return process.exit(0)
 }
 
 // Any preprocessing, etc...
